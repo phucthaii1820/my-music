d97e573bad7593774ec4f1ad499e536caaad4a8e
phucthaii1820 <phucthai0108@gmail.com>
2022-08-15 19:01:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, CardMedia, Grid, Typography } from "@mui/material";
+import React from "react";
+import ReactPlayer from "react-player";
+import "./App.css";
 
 function App() {
+  const [play, setPlay] = React.useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box
+      sx={{
+        minHeight: "100hv",
+        paddingTop: "100px",
+      }}
+    >
+      <Grid
+        container
+        spacing={5}
+        sx={{
+          paddingX: "100px",
+        }}
+      >
+        <Grid
+          item
+          xs={6}
+          sx={{
+            height: "600px",
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box
+            sx={{
+              height: "100%",
+              width: "100%",
+              backgroundColor: "white",
+              borderRadius: "20px",
+              padding: "50px",
+            }}
+          >
+            {/* <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexFlow: "column",
+              }}
+            >
+              <CardMedia
+                component="img"
+                image="https://phubuideptrai.github.io/nhaccuaphu/images/01.jpg"
+                alt="banner"
+                sx={{
+                  objectFit: "cover",
+                  width: "200px",
+                  height: "200px",
+                  borderRadius: play ? "100%" : "10px",
+                  animation: play ? "rotate 10s linear infinite" : null,
+                  "@keyframes rotate": {
+                    from: {
+                      transform: "rotate(0deg)",
+                    },
+                    to: {
+                      transform: "rotate(360deg)",
+                    },
+                  },
+                }}
+              />
+              <Typography
+                sx={{
+                  fontWeight: "700 !important",
+                  fontSize: "24px",
+                  mt: "40px",
+                }}
+              >
+                Mama Boy
+              </Typography>
+              <Typography
+                sx={{
+                  fontWeight: "600px",
+                  color: "rgb(144 144 144)",
+                }}
+              >
+                Amme
+              </Typography>
+            </Box>
+            <Button
+              onClick={() => {
+                setPlay(!play);
+              }}
+            >
+              Play
+            </Button> */}
+            <Box sx={{}}>
+              <ReactPlayer url="https://www.youtube.com/watch?v=4fkQP-UmNkE" />
+            </Box>
+          </Box>
+        </Grid>
+
+        <Grid
+          item
+          xs={6}
+          sx={{
+            height: "500px",
+          }}
+        >
+          <Box
+            sx={{
+              height: "100%",
+              width: "100%",
+              backgroundColor: "yellow",
+            }}
+          ></Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
